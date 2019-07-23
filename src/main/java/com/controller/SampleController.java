663a798c356e633b81b024f660ca80eba0405495
wanghj <wang@normee.co.jp>
2019-07-23 12:10:24 +0900

    add model attribute

--- src/main/java/com/controller/SampleController.java
+++ src/main/java/com/controller/SampleController.java
@@ -1,15 +1,22 @@
 package main.java.com.controller;
 
+import java.util.Date;
+
+import com.sun.tracing.dtrace.ModuleAttributes;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class SampleController {
 
     @RequestMapping(value = "/hello", method = GET)
-    public String hello() {
+    public String hello(Model model) {
         System.out.println("#############hello method#############");
+        Date date = new Date();
+        model.addAttribute("svrTime", date);
         // jspのファイル名
         return "test";
     }
